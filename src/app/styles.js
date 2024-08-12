6a6c153025d6bf8337de7010ce732e77468571d0
Ibrahim Shah <spaceboylux@gmail.com>
2024-08-12 18:22:13 +0300

    1st

---
+++ src/app/styles.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    padding: 2%;
+    max-width: 600px;
+    margin: 40px auto;
+    position: relative;
+    text-align: center;
+`;
+export const Heading = styled.h1`
+    text-align: center;
+    color: green;
+    font-size: 40px;
+`;
+
+export const Input1 = styled.input`
+    height: 50px;
+    width: 80%;
+    padding: 0;
+    font-size: 25px;
+    border-radius: 5px;
+`;
+export const Input2 = styled.input`
+    height: 50px;
+    width: 80%;
+    padding: 0;
+    font-size: 25px;
+    margin-top: 50px;
+    border-radius: 5px;
+`;
+
+export const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    position: relative;
+    // left: 30%;
+    margin-top: 10px;
+    cursor: pointer;
+    color: white;
+    background-color: green;
+    border-radius: 10px;
+`;
